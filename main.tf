d797da62a3d99a0af16a20665cd6213397066a63
kevinmaiden7 <kevin.m.gallego@gmail.com>
2020-11-14 11:03:04 -0500

    Cluster_AutoScalingGroup + ELB

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0e82959d4ed12de3f"
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0e82959d4ed12de3f"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -13,13 +13,32 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p 8080 &
                 EOF
 
-    tags = {
-        Name = "kevin-terraform"
+    lifecycle{
+        create_before_destroy = true
+    }
+}
+
+data "aws_availability_zones" "all" {
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    availability_zones = data.aws_availability_zones.all.names
+    load_balancers = [aws_elb.example.name]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 8
+
+    tag {
+        key = "Name"
+        value = "terraform-kevin-asg-instance"
+        propagate_at_launch = true
     }
 }
 
 resource "aws_security_group" "instance" {
-    name = "terraform-kevin-secgroup"
+    name = "terraform-kevin-asg-secgroup"
 
     ingress {
         from_port = 8080
@@ -27,4 +46,47 @@ resource "aws_security_group" "instance" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+resource "aws_elb" "example" {
+    name = "terraform-kevin-asg-elb"
+    availability_zones = data.aws_availability_zones.all.names
+    security_groups = [aws_security_group.elb.id]
+
+    listener {
+        lb_port = 80
+        lb_protocol = "http"
+        instance_port = 8080
+        instance_protocol = "http"
+    }
+
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3 # secs
+        interval = 30 # secs
+        target = "HTTP:8080/"
+    }
+}
+
+resource "aws_security_group" "elb" {
+    name = "terraform-kevin-elb"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+output "elb_dns_name" {
+    value = aws_elb.example.dns_name
 }
